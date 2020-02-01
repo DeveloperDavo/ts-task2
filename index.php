afafcff42f0dfda436726123985a47f33f652a0b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-02-01 12:51:42 +0100

    display response

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+$response = file_get_contents('https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json');
+echo $response
 ?>
 
 </body>
