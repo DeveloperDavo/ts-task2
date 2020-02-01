a515092e1d806dae7998f4f9024536a1bedf2001
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-02-01 12:57:34 +0100

    Display change date

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
 <?php
 $response = file_get_contents('https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json');
-echo $response
+$json = json_decode($response);
+print_r($json->response->data->shop->reviews['0']->changeDate);
 ?>
 
 </body>
