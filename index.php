337b2588d37d598d6bbf58b2c6582b8cfdce0a81
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-02-01 13:30:53 +0100

    Sort by mark descending

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 $response = file_get_contents('https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json');
 $json = json_decode($response);
 $reviews = $json->response->data->shop->reviews;
+
+function sortByMarkDescending($a, $b) {
+  return $b->mark - $a->mark;
+}
+usort($reviews, 'sortByMarkDescending');
+
 foreach ($reviews as $review) {
   echo ' ';
   echo $review->mark;
