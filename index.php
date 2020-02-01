b5f38876b288e4af4575e69120381676be20a427
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-02-01 13:04:30 +0100

    Display review marks

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 <?php
 $response = file_get_contents('https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json');
 $json = json_decode($response);
-print_r($json->response->data->shop->reviews['0']->changeDate);
+$reviews = $json->response->data->shop->reviews;
+foreach ($reviews as $review) {
+  echo ' ';
+  echo $review->mark;
+}
 ?>
 
 </body>
